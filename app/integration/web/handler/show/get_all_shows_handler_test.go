e2019191fa29a93f1b7457c4e75a28ac6b92afdc
stefnef <steffen@vorhundert.de>
2026-02-22 17:08:42 +0100

    implement handler

--- app/integration/web/handler/show/get_all_shows_handler_test.go
+++ app/integration/web/handler/show/get_all_shows_handler_test.go
@@ -2,9 +2,13 @@ package show
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
+	"net/http"
 	"net/http/httptest"
+	"podGopher/core/domain/model"
 	"podGopher/core/port/inbound"
+	"podGopher/core/port/inbound/show"
 	"podGopher/integration/web/handler"
 	"podGopher/integration/web/handler/handlerTestSetup"
 	"testing"
@@ -57,54 +61,107 @@ func Test_should_propagate_error_on_get_all_shows(t *testing.T) {
 	assert.Equal(t, expectedError, (*context.Errors[0]).Err)
 }
 
-//func Test_should_call_service_on_get_all_shows(t *testing.T) {
-//	defer mockGetShowService.init()
-//	var getShowDto *allShowsResponseDto
-//
-//	type testParameterStruct struct {
-//		title               string
-//		mockedPortResponse  *show.GetAllShowsResponse
-//		expectedWebResponse *allShowsResponseDto
-//	}
-//
-//	tests := []testParameterStruct{
-//		//TODO Beispiel mit nil und empty list füllen
-//		{
-//			"one show returned",
-//			&show.GetAllShowsResponse{
-//				Shows: []*model.Show{
-//					{
-//						Id:       "some-id",
-//						Title:    "Mocked Title",
-//						Slug:     "Mocked Slug",
-//						Episodes: []string{},
-//					},
-//				},
-//			},
-//			&allShowsResponseDto{
-//				//TODO Erwartung füllen
-//			},
-//		},
-//	}
-//
-//	for _, tc := range tests {
-//		var context, recorder = handlerTestSetup.GetTestGinContext(t)
-//		mockGetShowService.init()
-//
-//		t.Run(tc.title, func(t *testing.T) {
-//			mockGetShowService.returnsOnGetAllShow = tc.mockedPortResponse
-//
-//			context.Request = httptest.NewRequest("GET", "/show", bytes.NewBuffer([]byte("")))
-//
-//			getShowHandler.Handle(context)
-//
-//			var err = json.Unmarshal(recorder.Body.Bytes(), &getShowDto)
-//
-//			assert.Equal(t, 1, mockGetShowService.called)
-//			assert.Nil(t, err)
-//			assert.Empty(t, context.Errors)
-//			assert.Equal(t, tc.expectedWebResponse, getShowDto)
-//			assert.Equal(t, http.StatusOK, recorder.Code)
-//		})
-//	}
-//}
+func Test_should_call_service_on_get_all_shows(t *testing.T) {
+	defer mockGetShowService.init()
+	var getAllShowsDto *allShowsResponseDto
+
+	type testParameterStruct struct {
+		title               string
+		mockedPortResponse  *show.GetAllShowsResponse
+		expectedWebResponse *allShowsResponseDto
+	}
+
+	tests := []testParameterStruct{
+		{
+			"one show returned",
+			&show.GetAllShowsResponse{
+				Shows: []*model.Show{
+					{
+						Id:       "some-id",
+						Title:    "Mocked Title",
+						Slug:     "Mocked Slug",
+						Episodes: []string{},
+					},
+				},
+			},
+			&allShowsResponseDto{
+				Shows: []allShowsItemResponseDto{
+					{
+						Id:    "some-id",
+						Title: "Mocked Title",
+					},
+				},
+			},
+		},
+		{
+			"Nil response from port",
+			&show.GetAllShowsResponse{
+				Shows: nil,
+			},
+			&allShowsResponseDto{
+				Shows: []allShowsItemResponseDto{},
+			},
+		},
+		{
+			"Empty response from port",
+			&show.GetAllShowsResponse{
+				Shows: []*model.Show{},
+			},
+			&allShowsResponseDto{
+				Shows: []allShowsItemResponseDto{},
+			},
+		},
+		{
+			"2 shows returned",
+			&show.GetAllShowsResponse{
+				Shows: []*model.Show{
+					{
+						Id:       "some-id",
+						Title:    "Mocked Title",
+						Slug:     "Mocked Slug",
+						Episodes: []string{},
+					},
+					{
+						Id:       "2nd-show-id",
+						Title:    "Some other Title",
+						Slug:     "Some other Slug",
+						Episodes: []string{"some-episode-id"},
+					},
+				},
+			},
+			&allShowsResponseDto{
+				Shows: []allShowsItemResponseDto{
+					{
+						Id:    "some-id",
+						Title: "Mocked Title",
+					},
+					{
+						Id:    "2nd-show-id",
+						Title: "Some other Title",
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		var context, recorder = handlerTestSetup.GetTestGinContext(t)
+		mockGetShowService.init()
+
+		t.Run(tc.title, func(t *testing.T) {
+			mockGetShowService.returnsOnGetAllShow = tc.mockedPortResponse
+
+			context.Request = httptest.NewRequest("GET", "/show", bytes.NewBuffer([]byte("")))
+
+			getAllShowsHandler.Handle(context)
+
+			var err = json.Unmarshal(recorder.Body.Bytes(), &getAllShowsDto)
+
+			assert.Equal(t, 1, mockGetShowService.called)
+			assert.Nil(t, err)
+			assert.Empty(t, context.Errors)
+			assert.Equal(t, tc.expectedWebResponse, getAllShowsDto)
+			assert.Equal(t, http.StatusOK, recorder.Code)
+		})
+	}
+}
